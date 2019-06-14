e5d779e6047162df3b3ae47b7ff54e7d97799936
Christian Kaisermann <christian@kaisermann.me>
2019-06-14 15:50:30 -0300

    Move data into data directory

--- pug.config.js
+++ pug.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  self: true,
-  ...require("./app.json"),
-  asset(assetPath) {
-    if (process.env.SITE_URL) {
-      return `//${process.env.SITE_URL}/${assetPath}?c=${Math.random() *
-        10000}`;
-    }
+	self: true,
+	...require('./data/app.json'),
+	projects: require('./data/projects.json'),
+	asset(assetPath) {
+		if (process.env.SITE_URL) {
+			return `//${process.env.SITE_URL}/${assetPath}?c=${Math.random() *
+				10000}`;
+		}
 
-    return `${process.env.SITE_URL || "."}/${assetPath}`;
-  }
+		return `${process.env.SITE_URL || '.'}/${assetPath}`;
+	},
 };
