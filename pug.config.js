8e73b61f7d1ffee1f95b9b42a76e6972181b98b7
Christian Kaisermann <christian@kaisermann.me>
2020-06-30 14:55:44 -0300

    Use video as background

--- pug.config.js
+++ pug.config.js
@@ -2,10 +2,12 @@ module.exports = {
 	self: true,
 	...require('./data/app.json'),
 	projects: require('./data/projects.json'),
+	url: process.env.SITE_URL,
 	asset(assetPath) {
 		if (process.env.SITE_URL) {
-			return `//${process.env.SITE_URL}/${assetPath}?c=${Math.random() *
-				10000}`;
+			return `//${process.env.SITE_URL}/${assetPath}?c=${
+				Math.random() * 10000
+			}`;
 		}
 
 		return `${process.env.SITE_URL || '.'}/${assetPath}`;
