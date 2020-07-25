372f110d93797673565696754913d1782ffa8f9c
Christian Kaisermann <christian@kaisermann.me>
2020-07-25 17:27:28 -0300

    Lower the volume of videos

--- src/assets/scripts/Channel.svelte
+++ src/assets/scripts/Channel.svelte
@@ -7,7 +7,7 @@
 
   $: paddedNumber = number && number.toString().padStart(2, '0');
 
-  $: video && (video.volume = 0.3);
+  $: video && (video.volume = 0.32);
   $: {
     if (video && number != null) {
       video.load();
