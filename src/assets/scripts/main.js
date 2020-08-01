5ba1fa7261f38bb3eb8c3e89b90a4c4ebb88327f
Christian Kaisermann <christian@kaisermann.me>
2020-08-01 20:06:21 -0300

    Disable remote for now

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,10 @@
 import TV from './components/TV.svelte';
-import Remote from './components/Remote.svelte';
+// import Remote from './components/Remote.svelte';
 
 new TV({
   target: document.querySelector('.js-tv'),
 });
 
-new Remote({
-  target: document.querySelector('.js-remote'),
-});
+// new Remote({
+//   target: document.querySelector('.js-remote'),
+// });
