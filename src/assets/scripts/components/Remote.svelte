e223155ec8e8707dd3acccbdc1667e8573d81e82
Christian Kaisermann <christian@kaisermann.me>
2020-08-02 14:06:20 -0300

    wip 2

--- src/assets/scripts/components/Remote.svelte
+++ src/assets/scripts/components/Remote.svelte
@@ -50,40 +50,75 @@
     font-family: Arial, sans-serif;
     font-size: 13px;
     text-shadow: none;
-    padding: 50px 8px 80px;
-    background-color: #2d3436;
-    background-image: linear-gradient(315deg, #2d3436 0%, #d3d3d3 70%);
+    padding: 60px 8px 110px;
+    box-shadow: inset 0 14px 6px 0px #e0e0e0, inset 0 8px 10px 0px #252424,
+      inset 0px -11px 10px 0px #444;
+    background-color: #f1f2f6;
+    background: linear-gradient(to bottom right, #d8d2c9 0%, #d0cdc5 74%);
 
-    &,
-    &::before {
+    & {
       width: 320px;
-      height: 800px;
-      /* border-radius: 238px 238px 98px 98px / 58px 58px 188px 188px; */
-    }
-
-    &::before {
-      position: absolute;
-      z-index: -1;
-      content: '';
-      background: #212121;
-      top: 10px;
-      left: 0;
-      transform: scaleX(0.9);
+      height: 760px;
+      border-radius: 8px;
+      border-radius: 8px 8px 12px 12px / 8px 8px 24px 24px;
     }
   }
 
   .inner {
-    margin: 30px 8px 50px;
-    background-color: #ddd;
-    border-radius: 10px;
-    border: 20px solid #000;
+    margin: 40px 28px 50px;
     padding: 2.5em 15px 1em;
+    border-radius: 10px;
+    box-shadow: -20px -30px 0 0 #000, 20px 30px 0 0 #000, -20px 30px 0 0 #000,
+      20px -30px 0 0 #000, -22px -32px 0px 0 #696464, -22px 32px 0px 0 #696464,
+      22px -32px 0px 0 #696464, 22px 32px 0px 0 #696464,
+      inset 0px 0 6px 2px #646464, inset 0px 0 0 1px #000;
+    background-image: repeating-linear-gradient(
+        to bottom,
+        hsla(0, 0%, 100%, 0) 0%,
+        hsla(0, 0%, 100%, 0) 6%,
+        hsla(0, 0%, 100%, 0.08) 6.5%,
+        hsla(0, 0%, 100%, 0) 7%
+      ),
+      repeating-linear-gradient(
+        to bottom,
+        hsla(0, 0%, 0%, 0) 0%,
+        hsla(0, 0%, 0%, 0) 4%,
+        hsla(0, 0%, 0%, 0.03) 4.5%,
+        hsla(0, 0%, 0%, 0) 5%
+      ),
+      repeating-linear-gradient(
+        to bottom,
+        hsla(0, 0%, 100%, 0) 0%,
+        hsla(0, 0%, 100%, 0) 1.2%,
+        hsla(0, 0%, 100%, 0.15) 2.2%,
+        hsla(0, 0%, 100%, 0) 3%
+      ),
+      linear-gradient(
+        to bottom right,
+        hsl(0, 0%, 78%) 0%,
+        hsl(0, 0%, 90%) 47%,
+        hsl(0, 0%, 78%) 53%,
+        hsl(0, 0%, 90%) 100%
+      );
   }
 
   .buttons {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 45px 16px;
+    grid-template-columns: repeat(3, 35px);
+    justify-content: space-around;
+    row-gap: 36px;
+  }
+
+  .numbers {
+    margin: -23px -25px 23px;
+    padding: 22px;
+    grid-column: 1 / -1;
+    display: grid;
+    grid-template: inherit;
+    gap: inherit;
+    justify-content: space-between;
+    border: 3px solid #000;
+    border-radius: 10px;
   }
 
   .control {
@@ -97,37 +132,72 @@
       width: max-content;
       transform: translateX(-50%);
       color: #000;
+      margin-bottom: 0.4em;
     }
+  }
+
+  .hide-text {
+    font-size: 0;
+    text-indent: -999px;
+    color: transparent;
+  }
+
+  button {
+    cursor: url(../images/cursor-pointer.png) 14 8, auto;
+    width: 100%;
+    height: 25px;
+    border: 0px solid #333;
+    border-radius: 3px;
+    box-shadow: 1px 3px 0 0 #222, -1px 3px 0 0 #222;
+    font-size: 1.4em;
+    color: #fff;
+    background-color: #000000;
+    background-image: linear-gradient(315deg, #000000 0%, #414141 74%);
+
+    &:focus {
+      outline: none;
+    }
+
+    &:active {
+      border-width: 2px;
+      transform: translateY(2px);
+      box-shadow: 1px 1px 0 0 #222, -1px 1px 0 0 #222;
+    }
+  }
+
+  .onoff {
+    grid-area: 1/2;
 
     & button {
-      cursor: url(../images/cursor-pointer.png) 0 0, auto;
-      width: 35px;
-      height: 25px;
-      background-color: #807a72;
-      border: none;
-    }
-
-    &:not(.number) button {
-      font-size: 0;
-      text-indent: -999px;
-      color: transparent;
+      background-image: linear-gradient(315deg, #631111 0%, #dc2525 74%);
     }
   }
 
-  .numbers {
-    margin: -23px 0 23px;
-    grid-column: 1 / -1;
-    display: grid;
-    grid-template: inherit;
-    gap: inherit;
-    border: 3px solid #000;
-    border-radius: 10px;
-    padding: 22px 0;
+  .vol.up {
+    grid-area: 1/1;
   }
+  .vol.down {
+    grid-area: 2/1;
+  }
+  .mute {
+    grid-area: 2/2;
+  }
+  .ch.up {
+    grid-area: 1/3;
+  }
+  .ch.down {
+    grid-area: 2/3;
+  }
+  .showhide {
+    grid-area: 4/2/4/4;
 
-  .number button {
-    background-color: #000;
-    color: #fff;
+    & button {
+      font-size: 1em;
+      font-weight: bold;
+      width: 100%;
+      background-image: linear-gradient(315deg, #a71111 0%, #c32b2b 34%);
+      color: #fbcfcf;
+    }
   }
 
   .brand {
@@ -148,66 +218,63 @@
       <div class="inner">
         <div class="buttons">
           <div class="control onoff">
-            <button on:click={toggleOnOff}>ON / OFF</button>
-            <span>ON / OFF</span>
-          </div>
-          <div class="control showhide">
-            <button class="showhide" on:click={toggleContent}>
-              SHOW / HIDE
-            </button>
-            <span>SHOW / HIDE</span>
+            <button class="hide-text" on:click={toggleOnOff}>ON/OFF</button>
+            <span>OFF/ON</span>
           </div>
 
           <div class="control vol up">
-            <button on:click={increaseVolume}>VOL+</button>
-            <span>VOL+</span>
+            <button on:click={increaseVolume}>▲</button>
           </div>
 
           <div class="control vol down">
-            <button on:click={decreaseVolume}>VOL-</button>
-            <span>VOL</span>
+            <button on:click={decreaseVolume}>▼</button>
+            <span>VOLUME</span>
           </div>
 
           <div class="control mute">
-            <button on:click={toggleMute}>MUTE</button>
+            <button class="hide-text" on:click={toggleMute}>MUTE</button>
             <span>MUTE</span>
           </div>
 
           <div class="control ch up">
-            <button on:click={increaseChannel}>CH +</button>
-            <span>CH-</span>
+            <button on:click={increaseChannel}>▲</button>
           </div>
 
           <div class="control ch down">
-            <button on:click={decreaseChannel}>CH -</button>
-            <span>CH</span>
+            <button on:click={decreaseChannel}>▼</button>
+            <span>CHANNEL</span>
           </div>
 
           <div class="numbers">
             {#each { length: 9 } as _, i}
               <div class="control number">
-                <button on:click={decreaseChannel}>{i + 1}</button>
+                <button on:click={() => gotoChannel(i + 1)}>{i + 1}</button>
               </div>
             {/each}
             <div class="control number">
-              <button on:click={decreaseChannel}>0</button>
+              <button on:click={() => gotoChannel(0)}>0</button>
+            </div>
+            <div class="control showhide">
+              <button class="showhide" on:click={toggleContent}>
+                SHOW / HIDE
+              </button>
             </div>
           </div>
         </div>
 
         <div class="brand">
-          <img
+          <!-- <img
             src="/assets/images/kiwi.svg"
             alt="kiwi bird"
             width="30"
-            height="30" />
+            height="30" /> -->
           <img
             src="/assets/images/kiwivision.svg"
             alt="kiwivision"
             width="103"
             height="10" />
           <br />
-          <span>COMPUTER SPACE COMMANDER</span>
+          <span>COMPUTER SPACE COMMAND</span>
         </div>
       </div>
     </div>
