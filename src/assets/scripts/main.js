f571724613d5e421bf982fe439d10b1c9a0a0d13
Christian Kaisermann <christian@kaisermann.me>
2020-08-26 22:27:41 -0300

    Disable remote and external tv

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,15 +1,15 @@
 import TVScreen from './components/Screen.svelte';
-import Remote from './components/Remote.svelte';
-import RemoteTrigger from './components/RemoteTrigger.svelte';
+// import Remote from './components/Remote.svelte';
+// import RemoteTrigger from './components/RemoteTrigger.svelte';
 
 new TVScreen({
   target: document.querySelector('.js-screen'),
 });
 
-new Remote({
-  target: document.querySelector('.js-remote'),
-});
+// new Remote({
+//   target: document.querySelector('.js-remote'),
+// });
 
-new RemoteTrigger({
-  target: document.querySelector('.js-remote-trigger'),
-});
+// new RemoteTrigger({
+//   target: document.querySelector('.js-remote-trigger'),
+// });
