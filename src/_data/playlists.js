6d1fb2bf5674387c4c2c858314e4f864ecfb6b22
Christian Kaisermann <christian@kaisermann.me>
2020-09-03 22:47:46 -0300

    Rewrite initial home text

--- src/_data/playlists.js
+++ src/_data/playlists.js
@@ -39,32 +39,53 @@ async function getPlaylists(token) {
 }
 
 module.exports = async function fetchPlaylists() {
-  try {
-    const { access_token: token } = await getAccessToken();
-
-    const playlists = await getPlaylists(token);
-
-    const relevantPlaylists = playlists
-      .filter(
-        (p) => p.description.startsWith('@') && p.description.endsWith('@'),
-      )
-      .map((p) => {
-        return {
-          name: p.name,
-          url: p.external_urls.spotify,
-          description: p.description.slice(1, p.description.length - 1).trim(),
-          image: p.images[0].url,
-        };
-      });
-
-    return relevantPlaylists;
-  } catch (err) {
-    console.log('Something went wrong when retrieving an access token', err);
-
-    if (process.env.ELEVENTY_ENV === 'production') {
-      throw err;
-    }
-
-    return [];
+  if (process.env.ELEVENTY_ENV !== 'production') {
+    return [
+      {
+        name: 'フロー flow',
+        url: 'https://open.spotify.com/playlist/1oKT4BwzBziaqIs80UPbJq',
+        description: 'for when you know you&#x27;re going to code for a while',
+        image:
+          'https://i.scdn.co/image/ab67706c0000bebba02d8328058f5aa25064d0ce',
+      },
+      {
+        name: 'ローファイ  l o - f i ',
+        url: 'https://open.spotify.com/playlist/6ou1L5R6RrPk9XhWnls1Lt',
+        description: 'for any moment that you need to focus while head bobbing',
+        image:
+          'https://i.scdn.co/image/ab67706c0000bebb91471f262ee375f823633ee5',
+      },
+      {
+        name: '寛ぐ gaming nostalgia',
+        url: 'https://open.spotify.com/playlist/6aDjVGh6yqfGSevd8NBjMS',
+        description: 'for chilled nostalgic gaming moments',
+        image:
+          'https://i.scdn.co/image/ab67706c0000bebbc79a05be2cd83766bd94ec67',
+      },
+      {
+        name: 'p o s t - l i f e ',
+        url: 'https://open.spotify.com/playlist/1DQm54uVNgsBrCuDiie6b2',
+        description: 'post rock for contemplating life',
+        image:
+          'https://i.scdn.co/image/ab67706c0000bebb666bf1679f180a6ed8a26429',
+      },
+    ];
   }
+
+  const { access_token: token } = await getAccessToken();
+
+  const playlists = await getPlaylists(token);
+
+  const relevantPlaylists = playlists
+    .filter((p) => p.description.startsWith('@') && p.description.endsWith('@'))
+    .map((p) => {
+      return {
+        name: p.name,
+        url: p.external_urls.spotify,
+        description: p.description.slice(1, p.description.length - 1).trim(),
+        image: p.images[0].url,
+      };
+    });
+
+  return relevantPlaylists;
 };
