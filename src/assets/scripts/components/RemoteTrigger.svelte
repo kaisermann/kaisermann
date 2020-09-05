d0cb2e97ba7b7ece54ff6babd013739cdf723da6
Christian Kaisermann <christian@kaisermann.me>
2020-09-05 17:36:37 -0300

    Add secret remote mode

--- src/assets/scripts/components/RemoteTrigger.svelte
+++
@@ -1,9 +0,0 @@
-<button
-  class="remote-trigger cursor-pointer"
-  on:click={() => document.body.classList.toggle('remote')}>
-  <img
-    src="/assets/images/remote-control.svg"
-    alt="remote"
-    width="32"
-    height="32" />
-</button>
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,7 +1,6 @@
 import { init as initTextNav } from './modules/textNav.js';
 import TVScreen from './components/Screen.svelte';
-// import Remote from './components/Remote.svelte';
-// import RemoteTrigger from './components/RemoteTrigger.svelte';
+import Remote from './components/Remote.svelte';
 
 const bootstrap = () => {
   requestAnimationFrame(() => {
@@ -11,13 +10,7 @@ const bootstrap = () => {
 
     new TVScreen({ target: document.querySelector('.js-screen') });
 
-    // new Remote({
-    //   target: document.querySelector('.js-remote'),
-    // });
-
-    // new RemoteTrigger({
-    //   target: document.querySelector('.js-remote-trigger'),
-    // });
+    new Remote({ target: document.querySelector('.js-remote') });
   });
 };
 
