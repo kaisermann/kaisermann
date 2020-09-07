fd1819100721ba4f56d474aa169b215c9b197a24
Christian Kaisermann <christian@kaisermann.me>
2020-09-06 21:00:05 -0300

    Change background-position for a tranform

---
+++ src/assets/scripts/components/RemoteTrigger.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { toggleRemote } from '../tv.js';
+</script>
+
+<button class="remote-trigger cursor-pointer" on:click={() => toggleRemote()}>
+  <img
+    src="/assets/images/remote-control.svg"
+    alt="remote"
+    width="32"
+    height="32" />
+</button>
