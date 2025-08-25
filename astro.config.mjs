8608c4c831e04be003590735f8030d79e48cb217
Christian Kaisermann <christian@kaisermann.me>
2025-08-25 10:29:25 +0200

    Add env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
 
@@ -10,4 +10,19 @@ export default defineConfig({
   site: 'https://kaisermann.me',
 
   integrations: [sitemap(), svelte()],
+
+  env: {
+    schema: {
+      SPOTIFY_CLIENT_SECRET: envField.string({
+        default: '',
+        access: 'secret',
+        context: 'server',
+      }),
+      NODE_ENV: envField.string({
+        default: 'development',
+        access: 'public',
+        context: 'server',
+      }),
+    },
+  },
 })
